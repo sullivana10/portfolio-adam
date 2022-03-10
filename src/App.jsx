ccbeaa3b84d1322a8a6975bb10958e1a0044de6f
sullivana10 <sullivana10@gmail.com>
2022-03-10 16:15:40 -0600

    layout

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Skills from "./components/skills/Skills";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Skills />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,5 @@
+import "./skills.scss";
+
+export default function Skills() {
+  return <div className="skills"></div>;
+}
