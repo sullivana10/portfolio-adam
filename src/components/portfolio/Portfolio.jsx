5c581c2013b4ee1f1fc39ce9557a5bf4cdd73a4e
sullivana10 <sullivana10@gmail.com>
2022-03-11 00:54:58 -0600

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
 import "./portfolio.scss";
+import { portfolioData } from "../../data.js";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = portfolioData;
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Projects</h1>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <a href={d.link} target="_blank">
+              <img src={d.img} alt="" />
+            </a>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const portfolioData = [
+  {
+    id: 1,
+    title: "GigIntel",
+    link: "https://gigintel.herokuapp.com/",
+    img: "https://res.cloudinary.com/sullivana10/image/upload/v1646965241/GigIntelHomePage_by0ow2.png",
+  },
+  {
+    id: 2,
+    title: "Ecommerce",
+    link: "https://commerce-sullivan.netlify.app/",
+    img: "https://res.cloudinary.com/sullivana10/image/upload/v1646969124/Ecommerce_c9reup.png",
+  },
+  {
+    id: 3,
+    title: "Weather",
+    link: "https://weather-sullivan.netlify.app/",
+    img: "https://res.cloudinary.com/sullivana10/image/upload/v1646969206/Weather_hvo5hj.png",
+  },
+];
