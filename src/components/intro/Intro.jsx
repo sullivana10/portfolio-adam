d8a09921cf2ffe460740042283046da335a34caa
sullivana10 <sullivana10@gmail.com>
2022-03-10 19:36:43 -0600

    intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,23 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/PortfolioPic.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Adam Sullivan</h1>
+          <h3>Front End Web Developer</h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
