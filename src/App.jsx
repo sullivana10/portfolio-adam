e3d86276aef8a1f78a22b6e9a01dad8c6a8f8172
sullivana10 <sullivana10@gmail.com>
2022-03-10 19:03:37 -0600

    topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,31 @@
 import "./topbar.scss";
+import { LocalPhone, Email } from "@material-ui/icons";
 
-export default function Topbar() {
-  return <div className="topbar"></div>;
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Adam Sullivan
+          </a>
+          <div className="itemContainer">
+            <LocalPhone className="icon" />
+            <span>+1 612 568-4068</span>
+          </div>
+          <div className="itemContainer">
+            <Email className="icon" />
+            <span>AdamSullivanDev@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
