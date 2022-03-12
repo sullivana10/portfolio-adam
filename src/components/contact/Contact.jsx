84224582666faeeaaa03b90f79d74567ec602da2
sullivana10 <sullivana10@gmail.com>
2022-03-11 23:53:06 -0600

    skills

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,48 @@
+import emailjs from "emailjs-com";
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "portfolio_template",
+        e.target,
+        process.env.REACT_APP_EMAIL_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
+  return (
+    <div className="contact" id="contact">
+      <div className="container">
+        <div className="left">
+          <h2>Contact Me</h2>
+          <p>
+            Send me a message and I will get back to you as soon as i can.
+            Thanks!
+          </p>
+        </div>
+        <div className="right">
+          <form onSubmit={sendEmail}>
+            <input type="text" placeholder="Name" name="name" required></input>
+            <input type="text" placeholder="Email" name="email" required />
+            <input type="text" placeholder="Subject" name="subject" required />
+            <textarea placeholder="Message" name="message" required></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,28 @@
 import "./skills.scss";
+import HTML from "../../icons/HTML.svg";
+import js from "../../icons/js.svg";
+import bootstrap from "../../icons/bootstrap.svg";
+import sass from "../../icons/sass.svg";
+import react from "../../icons/react.svg";
+import git from "../../icons/git.svg";
 
 export default function Skills() {
-  return <div className="skills" id="skills"></div>;
+  return (
+    <div className="skills" id="skills">
+      <h1>Skills</h1>
+      <div className="icons">
+        <div className="top">
+          <img src={js} alt="" />
+          <img src={HTML} alt="" />
+          <img src={react} alt="" />
+        </div>
+
+        <div className="bottom">
+          <img src={sass} alt="" />
+          <img src={git} alt="" />
+          <img height={50} src={bootstrap} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
